ccc5e426275dbed185636d4f5025d533401c2e45
Stewart McKee <stewart@theizone.co.uk>
2010-11-18 13:14:35 +0000

    upped version and added begin resque for dropped connections

--- lib/cobweb.rb
+++ lib/cobweb.rb
@@ -61,44 +61,48 @@ class CobWeb
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end 
       request_time = Time.now.to_f
-      request = Net::HTTP::Get.new(uri.to_s)
-      response = http.request(request)
+      begin
+        request = Net::HTTP::Get.new(uri.to_s)
+        response = http.request(request)
   
-      if @options[:follow_redirects] and response.code.to_i >= 300 and response.code.to_i < 400
-        puts "redirected... " unless @options[:quiet]
-        url = response['location']
-        redirect_limit = redirect_limit - 1
-        content = get(response['location'], redirect_limit)
-        content[:url] = uri.to_s
-        content[:redirect_through] = [] if content[:redirect_through].nil?
-        content[:redirect_through].insert(0, response['location'])
-        
-        content[:response_time] = Time.now.to_f - request_time
-      else
-        content[:response_time] = Time.now.to_f - request_time
-        
-        puts "Retrieved." unless @options[:quiet]
-        
-        # create the content container
-        content[:url] = uri.to_s
-        content[:status_code] = response.code.to_i
-        content[:mime_type] = response.content_type
-        charset = response["Content-Type"][response["Content-Type"].index(";")+2..-1  ] if !response["Content-Type"].nil? and response["Content-Type"].include?(";")
-        charset = charset[charset.index("=")+1..-1] if charset and charset.include?("=")
-        content[:character_set] = charset 
-        content[:length] = response.content_length
-        content[:body] = response.body
-        content[:location] = response["location"]
-        content[:headers] = response.to_hash.symbolize_keys
-        # parse data for links
-        link_parser = ContentLinkParser.new(content[:url], content[:content_body])
-        content[:links] = link_parser.link_data
-        
-        # add content to cache if required
-        if @options[:cache]
-          redis.set(unique_id, content.to_json)
-          redis.expire unique_id, @options[:cache].to_i
+        if @options[:follow_redirects] and response.code.to_i >= 300 and response.code.to_i < 400
+          puts "redirected... " unless @options[:quiet]
+          url = response['location']
+          redirect_limit = redirect_limit - 1
+          content = get(response['location'], redirect_limit)
+          content[:url] = uri.to_s
+          content[:redirect_through] = [] if content[:redirect_through].nil?
+          content[:redirect_through].insert(0, response['location'])
+          
+          content[:response_time] = Time.now.to_f - request_time
+        else
+          content[:response_time] = Time.now.to_f - request_time
+          
+          puts "Retrieved." unless @options[:quiet]
+          
+          # create the content container
+          content[:url] = uri.to_s
+          content[:status_code] = response.code.to_i
+          content[:mime_type] = response.content_type
+          charset = response["Content-Type"][response["Content-Type"].index(";")+2..-1  ] if !response["Content-Type"].nil? and response["Content-Type"].include?(";")
+          charset = charset[charset.index("=")+1..-1] if charset and charset.include?("=")
+          content[:character_set] = charset 
+          content[:length] = response.content_length
+          content[:body] = response.body
+          content[:location] = response["location"]
+          content[:headers] = response.to_hash.symbolize_keys
+          # parse data for links
+          link_parser = ContentLinkParser.new(content[:url], content[:content_body])
+          content[:links] = link_parser.link_data
+          
+          # add content to cache if required
+          if @options[:cache]
+            redis.set(unique_id, content.to_json)
+            redis.expire unique_id, @options[:cache].to_i
+          end
         end
+      rescue
+        content = {}
       end
     end
     content  
