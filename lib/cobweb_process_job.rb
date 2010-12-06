68fbb3f9ae0a220c770cd79665d463c6a98ab609
Stewart McKee <stewart@theizone.co.uk>
2010-12-06 10:01:23 +0000

    moved content_process_job to cobweb_process_job

---
+++ lib/cobweb_process_job.rb
@@ -0,0 +1,13 @@
+class CobwebProcessJob
+  require "ap"
+  
+  @queue = :cobweb_process_job
+
+  def self.perform(content)
+    content.symbolize_keys 
+    puts "Dummy Processing for #{content[:url]}"
+
+    #ap content.keys
+    
+  end
+end
