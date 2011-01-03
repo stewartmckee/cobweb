c32e61eff4a0394d6863828c99e3fe1c2ffa285c
Stewart McKee <stewart@theizone.co.uk>
2011-01-03 16:56:38 +0000

    added head method

--- lib/cobweb.rb
+++ lib/cobweb.rb
@@ -17,7 +17,6 @@ class CobWeb
     @options[:debug] = false unless @options.has_key?(:debug)
     @options[:cache] = 300 unless @options.has_key?(:cache)
     @options[:timeout] = 10 unless @options.has_key?(:timeout)
-    
   end
   
   def start(base_url)
@@ -76,6 +75,7 @@ class CobWeb
         if @options[:follow_redirects] and response.code.to_i >= 300 and response.code.to_i < 400
           puts "redirected... " unless @options[:quiet]
           url = absolutize.url(response['location']).to_s
+          ap redirect_limit
           redirect_limit = redirect_limit - 1
           content = get(url, redirect_limit)
           content[:url] = uri.to_s
@@ -131,6 +131,80 @@ class CobWeb
       end
     end
     content  
+  end
+  
+  def head(url, redirect_limit = @options[:redirect_limit])
+    raise "url cannot be nil" if url.nil?    
+    
+    absolutize = Absolutize.new(url, :output_debug => false, :raise_exceptions => false, :force_escaping => false, :remove_anchors => true)
+    
+    # get the unique id for this request
+    unique_id = Digest::SHA1.hexdigest(url)
+    
+    # connect to redis
+    redis = NamespacedRedis.new(Redis.new, "cobweb")
+    
+    content = {}
+  
+    # check if it has already been cached
+    if redis.get(unique_id) and @options[:cache]
+      puts "Cache hit for #{url}" unless @options[:quiet]
+      content = JSON.parse(redis.get(unique_id)).deep_symbolize_keys
+      content[:body] = Base64.decode64(content[:body]) unless content[:mime_type].include?("text/html") or content[:mime_type].include?("application/xhtml+xml")
+
+      content
+    else
+      print "Retrieving #{url }... " unless @options[:quiet]
+      uri = URI.parse(url)
+      
+      # retrieve data
+      http = Net::HTTP.new(uri.host, uri.port)
+      if uri.scheme == "https"
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end 
+  
+      request_time = Time.now.to_f
+      http.read_timeout = @options[:timeout].to_i
+      http.open_timeout = @options[:timeout].to_i
+    
+      begin      
+        response = http.head(uri.to_s)
+        
+        if @options[:follow_redirects] and response.code.to_i >= 300 and response.code.to_i < 400
+          puts "redirected... " unless @options[:quiet]
+          url = absolutize.url(response['location']).to_s
+          redirect_limit = redirect_limit - 1
+          content = head(url, redirect_limit)
+          content[:url] = uri.to_s
+          content[:redirect_through] = [] if content[:redirect_through].nil?
+          content[:redirect_through].insert(0, url)
+         else
+          content[:url] = uri.to_s
+          content[:status_code] = response.code.to_i
+          content[:mime_type] = response.content_type.split(";")[0].strip
+          charset = response["Content-Type"][response["Content-Type"].index(";")+2..-1] if !response["Content-Type"].nil? and response["Content-Type"].include?(";")
+          charset = charset[charset.index("=")+1..-1] if charset and charset.include?("=")
+          content[:character_set] = charset 
+        end
+      rescue Exception => e
+        puts "ERROR: #{e.message}"        
+        
+        ## generate a blank content
+        content = {}
+        content[:url] = uri.to_s
+        content[:response_time] = Time.now.to_f - request_time
+        content[:status_code] = 0
+        content[:length] = 0
+        content[:body] = ""
+        content[:error] = e.message
+        content[:mime_type] = "internal/serverdown"
+        content[:headers] = {}
+        content[:links] = {}
+      end
+      
+      content
+    end
   end
 end
 
