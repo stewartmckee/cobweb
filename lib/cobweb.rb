9b929b285e3b326747f895bdfd5093de9f3aa799
Stewart McKee <stewart@theizone.co.uk>
2012-02-27 22:56:14 +0000

    changing queue and crawl lists to redis

--- lib/cobweb.rb
+++ lib/cobweb.rb
@@ -110,7 +110,7 @@ class Cobweb
           content[:url] = uri.to_s
           content[:status_code] = response.code.to_i
           content[:mime_type] = response.content_type.split(";")[0].strip unless response.content_type.nil?
-          if response["Content-Type"].include? ";"
+          if !response["Content-Type"].nil? && response["Content-Type"].include? ";"
             charset = response["Content-Type"][response["Content-Type"].index(";")+2..-1] if !response["Content-Type"].nil? and response["Content-Type"].include?(";")
             charset = charset[charset.index("=")+1..-1] if charset and charset.include?("=")
             content[:character_set] = charset
