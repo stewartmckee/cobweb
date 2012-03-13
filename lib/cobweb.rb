7c395eb8f7c0c445eba4f735b33614862c5eec5b
Stewart McKee <stewart@theizone.co.uk>
2012-03-13 22:36:38 +1000

    upped version

--- lib/cobweb.rb
+++ lib/cobweb.rb
@@ -19,7 +19,7 @@ class Cobweb
   # investigate using event machine for single threaded crawling
   
   def self.version
-    "0.0.25"
+    "0.0.26"
   end
   
   def method_missing(method_sym, *arguments, &block)
