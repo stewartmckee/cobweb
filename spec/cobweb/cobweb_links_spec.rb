7dbc3b0c4051a73449619ea9b65fe8e9343c929d
Stewart McKee <stewart@theizone.co.uk>
2012-05-07 17:27:31 +0100

    fixed typo in specs

--- spec/cobweb/cobweb_links_spec.rb
+++ spec/cobweb/cobweb_links_spec.rb
@@ -19,37 +19,37 @@ describe CobwebLinks do
 
   
   it "should generate a cobweb_links object" do
-    CobwebLinks.new(:internal_links => [""]).should be_an_instance_of CobwebLinks
+    CobwebLinks.new(:internal_urls => [""]).should be_an_instance_of CobwebLinks
   end
 
   it "should raise error with no internal links" do
     lambda{CobwebLinks.new()}.should raise_error
   end    
   it "should not raise error with missing external links" do
-    lambda{CobwebLinks.new(:internal_links => ["http://domain_one.com/"])}.should_not raise_error
+    lambda{CobwebLinks.new(:internal_urls => ["http://domain_one.com/"])}.should_not raise_error
   end
   it "should raise error with invalid internal links" do
-    lambda{CobwebLinks.new(:internal_links => "")}.should raise_error
+    lambda{CobwebLinks.new(:internal_urls => "")}.should raise_error
   end
   it "should raise error with invalid external links" do
-    lambda{CobwebLinks.new(:internal_links => [], :external_links => "")}.should raise_error
+    lambda{CobwebLinks.new(:internal_urls => [], :external_urls => "")}.should raise_error
   end
     
   
   describe "internal and external links" do
     it "should only return internal links" do
-      cobweb_links = CobwebLinks.new(:internal_links => ["http://domain_one.com/"], :external_links => ["http://domain_two.com/"])
+      cobweb_links = CobwebLinks.new(:internal_urls => ["http://domain_one.com/"], :external_urls => ["http://domain_two.com/"])
       cobweb_links.internal?("http://domain_one.com/pageone.html").should be_true
       cobweb_links.internal?("http://domain_one.com/pagetwo.html").should be_true
     end
     it "should not return external links" do
-      cobweb_links = CobwebLinks.new(:internal_links => ["http://domain_one.com/"], :external_links => ["http://domain_two.com/"])
+      cobweb_links = CobwebLinks.new(:internal_urls => ["http://domain_one.com/"], :external_urls => ["http://domain_two.com/"])
       cobweb_links.external?("http://domain_one.com/pageone.html").should be_false
       cobweb_links.external?("http://domain_two.com/pageone.html").should be_true      
       cobweb_links.external?("http://external.com/pageone.html").should be_true      
     end
     it "should override internal links with external links" do
-      cobweb_links = CobwebLinks.new(:internal_links => ["http://domain_one.com/"], :external_links => ["http://domain_one.com/blog"])
+      cobweb_links = CobwebLinks.new(:internal_urls => ["http://domain_one.com/"], :external_urls => ["http://domain_one.com/blog"])
       cobweb_links.internal?("http://domain_one.com/pageone.html").should be_true
       cobweb_links.external?("http://domain_one.com/pageone.html").should be_false
       cobweb_links.internal?("http://domain_one.com/blog/pageone.html").should be_false
@@ -59,7 +59,7 @@ describe CobwebLinks do
     end
   end
   it "should only match from beginning of url" do
-    cobweb_links = CobwebLinks.new(:internal_links => ["http://www.domain_one.com/"], :external_links => ["http://www.domain_two.com/"])
+    cobweb_links = CobwebLinks.new(:internal_urls => ["http://www.domain_one.com/"], :external_urls => ["http://www.domain_two.com/"])
     cobweb_links.internal?("http://www.domain_one.com/pageone.html").should be_true
     cobweb_links.internal?("http://www.domain_two.com/pageone.html").should be_false
     cobweb_links.internal?("http://www.domain_one.com/pageone.html?url=http://www.domain_two.com/pageone.html").should be_true
@@ -68,26 +68,26 @@ describe CobwebLinks do
   
   describe "using wildcards" do
     it "should match internal links with wildcards" do
-      cobweb_links = CobwebLinks.new(:internal_links => ["http://*.domain_one.com/"], :external_links => ["http://blog.domain_one.com/"])
+      cobweb_links = CobwebLinks.new(:internal_urls => ["http://*.domain_one.com/"], :external_urls => ["http://blog.domain_one.com/"])
       cobweb_links.internal?("http://www.domain_one.com/pageone.html").should be_true
       cobweb_links.internal?("http://images.domain_one.com/logo.png").should be_true      
       cobweb_links.internal?("http://blog.domain_one.com/pageone.html").should be_false
     end
     it "should match external links with wildcards" do
-      cobweb_links = CobwebLinks.new(:internal_links => ["http://www.domain_one.com/"], :external_links => ["http://*.domain_one.com/"])
+      cobweb_links = CobwebLinks.new(:internal_urls => ["http://www.domain_one.com/"], :external_urls => ["http://*.domain_one.com/"])
       cobweb_links.internal?("http://www.domain_one.com/pageone.html").should be_false
       cobweb_links.internal?("http://images.domain_one.com/logo.png").should be_false
       cobweb_links.internal?("http://blog.domain_one.com/pageone.html").should be_false
     end
     it "should allow multiple wildcards" do
-      cobweb_links = CobwebLinks.new(:internal_links => ["http://*.*.domain_one.com/"])
+      cobweb_links = CobwebLinks.new(:internal_urls => ["http://*.*.domain_one.com/"])
       cobweb_links.internal?("http://www.domain_one.com/pageone.html").should be_false
       cobweb_links.internal?("http://blog.domain_one.com/pageone.html").should be_false
       cobweb_links.internal?("http://www.marketing.domain_one.com/pageone.html").should be_true
       cobweb_links.internal?("http://blog.designers.domain_one.com/pagetwo.html").should be_true      
     end
     it "should allow multiple country tlds with wildcards" do
-      cobweb_links = CobwebLinks.new(:internal_links => ["http://*.domain_one.*/", "http://*.domain_one.*.*/"])
+      cobweb_links = CobwebLinks.new(:internal_urls => ["http://*.domain_one.*/", "http://*.domain_one.*.*/"])
       cobweb_links.internal?("http://www.domain_one.com/pageone.html").should be_true
       cobweb_links.internal?("http://blog.domain_one.com/pageone.html").should be_true
       cobweb_links.internal?("http://www.domain_one.co.uk/pageone.html").should be_true
