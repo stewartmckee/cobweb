abf98f113f3046da4477365a8c2a13fcec9246c9
Stewart McKee <stewart@theizone.co.uk>
2012-08-20 01:44:22 +0100

    fixed bug when option being passed in wasn't string

--- lib/stats.rb
+++ lib/stats.rb
@@ -14,7 +14,7 @@ class Stats
     unless @full_redis.sismember "cobweb_crawls", options[:crawl_id]
       @full_redis.sadd "cobweb_crawls", options[:crawl_id]
       options.keys.each do |key|
-        @redis.hset "crawl_details", key, options[key]
+        @redis.hset "crawl_details", key, options[key].to_s
       end
     end
     @redis.hset "statistics", "current_status", "Crawl Starting..."
